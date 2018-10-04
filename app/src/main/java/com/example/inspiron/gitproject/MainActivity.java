37d5c5d5eeeb87674375d2efac9a5edd7044450a
Swaraj <svj.ravi@gmail.com>
2018-10-05 00:02:53 +0530

    Third commit

--- app/src/main/java/com/example/inspiron/gitproject/MainActivity.java
+++ app/src/main/java/com/example/inspiron/gitproject/MainActivity.java
@@ -1,10 +1,28 @@
 package com.example.inspiron.gitproject;
 
+import android.content.Context;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
-    int s;
+    WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+    WifiInfo wInfo = wifiManager.getConnectionInfo();
+    String macAddress= "initial";
+
+    WifiManager mWifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+//    WifiManager mWifiManager = (WifiManager) this.getSystemService(Context.WIFI_SERVICE);
+        mWifiManager.startScan()
+    //    mWifiManager.startScan();
+    List<ScanResult> results = mWifiManager.getScanResults();
+        for (ScanResult result : results)
+    {
+        System.out.println("Access Point MacAddr:" + result.BSSID);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
